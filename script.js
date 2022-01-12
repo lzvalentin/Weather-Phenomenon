eb02e30d9f4198f2641d033c3ddbec100929e457
Lorena Zuniga <lzuniga1315@gmail.com>
2022-01-11 22:42:40 -0800

    added api and html

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// set var
+ let weather= {
+ apiKey = "928c5ba094c06154ca53619a766d8d60",
+fetchWeather: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "units=imperial&appid=" + apiKey)
+.then((response) => response.json())
+    .then(data => console.log(data));
+}},
 
-
-
-
-
-
-
-
-
-// console log
